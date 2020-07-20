1a187bf25d524a4f44b6252103514f9c01014b55
Joshua Ogbonna <devjaykes@gmail.com>
2020-07-20 17:02:55 +0100

    First commit for the Ghana Votes

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,20 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Login from '@/views/Login.vue'
+import Redirect from '@/views/Redirect.vue'
+import { auth } from '@/views/firebase.js'
 
 Vue.use(VueRouter)
 
-  const routes = [
+const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    meta: {
+      requiresAuth: true
+    }
   },
   {
     path: '/about',
@@ -16,7 +22,20 @@ Vue.use(VueRouter)
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    meta: {
+      requiresAuth: true
+    }
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login
+  },
+  {
+    path: '*',
+    name: 'redirect',
+    component: Redirect
   }
 ]
 
@@ -24,6 +43,16 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  const requiresAuth = to.matched.some(x => x.meta.requiresAuth)
+
+  if (requiresAuth && !auth.currentUser) {
+    next('login')
+  } else {
+    next()
+  }
 })
 
 export default router
---
+++ src/views/firebase.js
@@ -0,0 +1,20 @@
+import * as firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyD_7wX4MRmcPzaD8k-tshLzXYwtabV2WAs',
+  authDomain: 'ghana-votes.firebaseapp.com',
+  databaseURL: 'https://ghana-votes.firebaseio.com',
+  projectId: 'ghana-votes',
+  storageBucket: 'ghana-votes.appspot.com',
+  messagingSenderId: '122165507341',
+  appId: '1:122165507341:web:1a663ef3546a7595f8ec44',
+  measurementId: 'G-T6T0EL0X5F'
+}
+
+firebase.initializeApp(firebaseConfig)
+
+const auth = firebase.auth()
+
+export { auth }
